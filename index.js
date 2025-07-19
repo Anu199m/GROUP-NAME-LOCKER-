c4960d0e3b11b3767db176566e68b998ab710585
Anu199m <92900219+Anu199m@users.noreply.github.com>
2025-07-19 15:07:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 
 const appState = JSON.parse(fs.readFileSync("appstate.json", "utf-8"));
 
-const GROUP_THREAD_ID = "24041654888825173";
-const LOCKED_GROUP_NAME = "TUSHAR KE PAPA AVI HERE:)";
+const GROUP_THREAD_ID = "9909298062530889";
+const LOCKED_GROUP_NAME = "NAME CHANGER TESTING:)";
 
 login({ appState }, (err, api) => {
   if (err) return console.error("Login Failed:", err);
